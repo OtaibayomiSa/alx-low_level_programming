2c795ebb977902443d9e20cedd53fb140965a6f9
OtaibayomiSa <fran6koabayomi@gmail.com>
2023-10-05 12:39:08 +0100

    C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ *
+ * return: Always 0
+ */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
